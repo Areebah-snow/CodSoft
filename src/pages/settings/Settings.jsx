f8f743fb4609d7ecfa29666a645b499a5d13db1f
Areebah-snow <mudathiraisha657@gmail.com>
2023-09-10 23:11:24 +0100

    new styling

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -5,7 +5,36 @@ const Settings = () => {
     return (
         <div className="settings">
             <div className="settingWrapper">
-                
+                <div className="settingsProfile">
+                    <span className="settingsUpdateProfile">
+                     Update Your Account
+                    </span>
+                    <span className="settingsUpdateProfile">
+                    Delete Account
+                    </span>
+
+                </div>
+                <form action="" className="settingsForm">
+                    <label htmlFor="">Profile Picture</label>
+                    <div className="settingsProfilePicture">
+                        <img 
+                        src="https://images.pexels.com/photos/1058959/pexels-photo-1058959.jpeg?auto=compress&cs=tinysrgb&w=600" 
+                        alt="" />
+
+                        <label htmlFor="FileInput">
+                        <i class="fa-regular fa-circle-user SettingsProfileIcon"></i>
+                        </label>
+                        <input type="file" id="fileInput" style={{display:"none"}}/>
+                    </div>
+                    <label htmlFor="">Username</label>
+                    <input type="text" placeholder='Mary' />
+                    <label htmlFor="">Email</label>
+                    <input type="email" placeholder='maryanne@gmail.com' />
+                    <label htmlFor="">Password</label>
+                    <input type="password"/>
+
+                    <button className="settingsButton">Update</button>
+             </form>
             </div>
         </div>
     )
